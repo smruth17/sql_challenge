79336264f5d9c37c4ba931fca331b676a2c9e9a9
Sarah_Ruth <sruth77@yahoo.com>
2024-06-22 14:42:28 -0500

    HW9

--- EmployeesQL/QuickDBD-WK9_HW.sql
+++ EmployeesQL/QuickDBD-WK9_HW.sql
@@ -108,12 +108,15 @@ ORDER BY
 
 
 -- 3. List the manager of each department along with their department number, department name, employee number, last name, and first name.
-SELECT
-	
-FROM
-	
-
-
+SELECT 
+	d.dept_no,
+	d.dept_name,
+	e.emp_no,
+	e.last_name,
+	e.first_name
+FROM employees e
+JOIN dept_manager dm ON e.emp_no = dm.emp_no
+JOIN departments d ON d.dept_no = dm.dept_no;
 
 -- 4. List the department number for each employee along with that employee’s employee number, last name, first name, and department name.
 SELECT 
@@ -145,7 +148,37 @@ ORDER BY
 
 -- 6. List each employee in the Sales department, including their employee number, last name, and first name.
 
+SELECT
+	e.emp_no,
+	e.last_name,
+	e.first_name
+FROM 
+	employees e
+
+JOIN dept_emp de ON e.emp_no = de.emp_no
+
+WHERE 
+	de.dept_no = 'd007'
+ORDER BY 
+	e. last_name ASC;
+
+
+
 -- 7. List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name.
+SELECT
+	e.emp_no,
+	e.last_name,
+	e.first_name,
+	d.dept_name
+FROM 
+	employees e
+
+JOIN dept_emp de ON e.emp_no = de.emp_no
+JOIN departments d ON de.dept_no = d.dept_no
+WHERE 
+	d.dept_name IN ('Sales','Development')
+ORDER BY 
+	e. last_name ASC;
 
 -- 8. List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name).
 SELECT
